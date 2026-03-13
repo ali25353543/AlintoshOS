bb837fa250963ac4595c5a6b13e8acecf6665d6f
Alintosh <hajoraabdo85@gmail.com>
2026-03-13 23:47:04 +0200

    Enhance build.sh for compiling and running kernel
    Updated build script to compile C and assembly files, link objects, and run with QEMU.

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-if command -v qemu-system >/dev/null 2>&1; then
-else
-     apt-get install qemu-system
-     
-fi
+objects="loader.o kmain.o io.o fb.o serial.o gdt.o gdt_s.o idt.o idt_s.o keyboard.o shell.o snake.o"
+cflags="-m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -nodefaultlibs -Wall -Wextra -Werror -c"
+ldflags="-T link.ld -melf_i386"
+for "$a" in "*.c" do
+     gcc "$cflags" "$a" -o "${%a}.o"
+done
+for "$a" in "*.asm" do
+     nasm -f elf "$a" -o "${%a}.o"
+done
+ld "$ldflags" "$objects" -o kernel.elf
+qemu-system-x86_64 -kernel kernel.elf
