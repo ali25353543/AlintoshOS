858b765115aab0794154cd4ac81d44374077d9fa
Alintosh <hajoraabdo85@gmail.com>
2026-03-14 00:33:50 +0200

    Add build.bat for kernel compilation and execution
    This batch file compiles and links kernel components for an OS project, checking for necessary tools and handling errors.

---
+++ build.bat
@@ -0,0 +1,32 @@
+@echo off
+:: just for testing
+set objects=loader.o kmain.o io.o fb.o serial.o gdt.o gdt_asm.o idt.o idt_asm.o keyboard.o shell.o snake.o
+set asflags=-f elf
+set cflags=-m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -nodefaultlibs -Wall -Wextra -Werror -c
+set ldflags=-T link.ld -melf_i386
+i686-elf-gcc
+if %errorlevel%==9009 (
+  echo install i686-elf-tools
+  goto :A
+) else if %errorlevel%==1 (
+  for %a in ( "*.c" ) do (
+    i686-elf-gcc %cflags% %a -o %~na.o
+  )
+)
+:A
+nasm
+if %errorlevel%==9009 (
+  echo install nasm
+  goto :B
+) else if %errorlevel%==1 (
+  for %a in ( "*.asm" ) do (
+    nasm %asflags% %a -o %a~na.o
+  )
+)
+i686-elf-ld %ldflags% %objects% -o kernel.elf
+:B
+qemu-system-x86_64 -kernel kernel.elf
+if %errorlevel%==9009 (
+  echo install qemu
+  exit /b 1
+)
